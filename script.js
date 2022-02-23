f58a5a2d181643e291b7377db1f8113ed63e599f
ElChiChe <orellanasebastian15@gmail.com>
2022-02-23 13:52:42 -0300

    challenge completed

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const url = "https://api.adviceslip.com/advice";
 
+const text_span = document.querySelector(".id-span");
+const advice_text = document.querySelector(".advice-text");
+const dice_icon = document.querySelector(".dice-icon");
 
+const requestAdvice = () => {
 fetch(url)
-.then(res => res.json())
-.then(data => console.log(data.slip.advice));
+	.then(res => res.json())
+	.then(data => {
+		let advice_id = data.slip.id;
+		let advice = data.slip.advice;
+	
+		text_span.textContent = advice_id;
+		advice_text.textContent = `"${advice}"` 
+	})
+}
+
+dice_icon.addEventListener("click", requestAdvice);
